e473b1c9f75cf4edcc2e881f08e48ec9b3239395
supersu <supersu@alibaba.com>
2020-09-01 18:44:12 +0800

    do git commit

---
+++ src/main/java/com/atguigu/github/GitDemo.java
@@ -0,0 +1,17 @@
+package com.atguigu.github;
+
+/**
+ * @author Suchao Zou
+ * @create 2020-09-01 18:40
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+        System.out.println("Hello PHP");
+        System.out.println("Hello python");
+        System.out.println("Hello scala");
+        System.out.println("Hello C");
+        System.out.println("Hello C++");
+        System.out.println("Hello C#");
+    }
+}
