1ee2623b4a17ba88938166933c013790ce36b614
supersu <supersu@alibaba.com>
2020-09-01 19:12:10 +0800

    Test conflict by hotfix

--- src/main/java/com/atguigu/github/GitDemo2.java
+++ src/main/java/com/atguigu/github/GitDemo2.java
@@ -7,7 +7,7 @@ package com.atguigu.github;
 public class GitDemo2 {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello");//Test php by hotfix
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
